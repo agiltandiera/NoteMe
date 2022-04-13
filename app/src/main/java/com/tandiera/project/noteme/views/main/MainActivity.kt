34ab80a00d870e5095a8465efeafea5250ca55b0
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-04-13 12:13:05 +0800

    create bottom_navigation_colored

--- app/src/main/java/com/tandiera/project/noteme/views/main/MainActivity.kt
+++ app/src/main/java/com/tandiera/project/noteme/views/main/MainActivity.kt
@@ -3,10 +3,21 @@ package com.tandiera.project.noteme.views.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tandiera.project.noteme.R
+import com.tandiera.project.noteme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.btmNavMain
     }
 }
