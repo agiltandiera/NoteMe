969393b567c66ce08e746d9595880a491b670fb4
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-04-22 10:48:04 +0800

    create DbHelper

--- app/src/main/java/com/tandiera/project/noteme/db/DbContract.kt
+++ app/src/main/java/com/tandiera/project/noteme/db/DbContract.kt
@@ -3,7 +3,7 @@ package com.tandiera.project.noteme.db
 object DbContract {
     class MyTask {
         companion object {
-            const val TABEL_TASK = "task"
+            const val TABLE_TASK = "task"
             const val TASK_ID = "task_id"
             const val TASK_TITLE = "task_title"
             const val TASK_DETAILS = "task_details"
