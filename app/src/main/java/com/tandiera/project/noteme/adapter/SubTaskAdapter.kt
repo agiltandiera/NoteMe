80617b2d8450cf3a9bae54aa86a2edbe151f7f95
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-04-25 06:43:30 +0800

    send data to New Activity

--- app/src/main/java/com/tandiera/project/noteme/adapter/SubTaskAdapter.kt
+++ app/src/main/java/com/tandiera/project/noteme/adapter/SubTaskAdapter.kt
@@ -2,6 +2,7 @@ package com.tandiera.project.noteme.adapter
 
 import android.graphics.Paint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tandiera.project.noteme.R
@@ -11,6 +12,7 @@ import com.tandiera.project.noteme.model.SubTask
 class SubTaskAdapter : RecyclerView.Adapter<SubTaskAdapter.ViewHolder> () {
 
     private lateinit var subTasks :  List<SubTask>
+    private lateinit var listener: (View) -> Unit
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
         val binding = ItemSubTaskBinding
@@ -19,7 +21,7 @@ class SubTaskAdapter : RecyclerView.Adapter<SubTaskAdapter.ViewHolder> () {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(subTasks[position])
+        holder.bind(subTasks[position], listener)
     }
 
     override fun getItemCount(): Int = subTasks.size
@@ -29,9 +31,13 @@ class SubTaskAdapter : RecyclerView.Adapter<SubTaskAdapter.ViewHolder> () {
         notifyDataSetChanged()
     }
 
+    fun onClick(listener: (View) -> Unit) {
+        this.listener = listener
+    }
+
     class ViewHolder(val binding: ItemSubTaskBinding)
         : RecyclerView.ViewHolder(binding.root) {
-        fun bind(subTask: SubTask) {
+        fun bind(subTask: SubTask, listener: (View) -> Unit) {
             binding.tvTitleSubTask.text = subTask.title
 
             if(subTask.isComplete) {
@@ -49,6 +55,11 @@ class SubTaskAdapter : RecyclerView.Adapter<SubTaskAdapter.ViewHolder> () {
                     subTask.isComplete = true
                 }
             }
+
+            itemView.setOnClickListener {
+                listener(it)
+            }
+
         }
 
         private fun completeSubTask() {
